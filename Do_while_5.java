4b4cef59d4802f8996e4f353b2d786fa59a83dfe
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2023-04-02 13:28:31 +0530

    Create Do_while with no error

---
+++ Do_while_5.java
@@ -0,0 +1,10 @@
+public class Do_while_5 {
+    public static void main(String[] args) {
+        int i=0;
+        // Do while Loop executes statement until certain condition becomes false
+        do {
+            System.out.println("i is :" + i);
+            i++;
+        } while (i > 7);
+    }
+}
