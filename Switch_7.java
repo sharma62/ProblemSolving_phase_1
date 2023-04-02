0001d86b5aa1797d8b9655c075578253d8bb710d
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2023-04-03 01:37:05 +0530

    Switch_7.java create without error

---
+++ Switch_7.java
@@ -0,0 +1,20 @@
+public class Switch_7 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i is 0");
+                    break;
+                case 1:
+                    System.out.println("i is 2");
+                    break;
+                case 2:
+                    System.out.println("i is 2");
+                    break;
+
+                default:
+                    System.out.println("i is more then 2");
+            }
+        }
+    }
+}
