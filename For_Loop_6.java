da1d8dc7e8526e06fd067bf87ea73a03dcb7504b
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2023-04-02 13:31:09 +0530

    add For_Loop 6th example

---
+++ For_Loop_6.java
@@ -0,0 +1,8 @@
+public class For_Loop_6 {
+    public static void main(String[] args) {
+         for (int i = 0; i < args.length; i++) {
+            System.out.println("Hello " + i);
+            
+        }
+    }
+}
