0175f36a5cbbfa8cd7e51c43d80e98d7998bca26
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2023-04-04 23:51:34 +0530

    complete prime number logic

--- PrimeNumber_11.java
+++ PrimeNumber_11.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 /**
  * PrimeNumber_11
  */
 public class PrimeNumber_11 {
 
     public static void main(String[] args) {
-        
+        int n, status = 1, num = 3;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of prime numbers you want");
+        n = input.nextInt();
+        if (n >= 1) {
+            System.out.println("first" + n + "Prime number are :");
+            System.out.println(2);
+        }
+
+        for (int count = 0; count <= n;) {
+            for (int i = 0; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    status = 0;
+                    break;
+                }
+            }
+            if (status != 0) {
+                System.out.println(num);
+                count++;
+            }
+            status = 1;
+            num++;
+        }
     }
 }
