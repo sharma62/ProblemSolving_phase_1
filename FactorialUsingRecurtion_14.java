a508cf6da1f43128f14872c93ca7d5e56a2b9bfe
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2023-04-06 23:42:18 +0530

    solve  14th problem 'factorial using recurtion'

--- FactorialUsingRecurtion_14.java
+++ FactorialUsingRecurtion_14.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class FactorialUsingRecurtion_14 {
-    
+    public static void main(String[] args) throws NumberFormatException,IOException {
+        System.out.println("Enter an integer to calculate its factrial ");
+        //get input from the user
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in);
+        int a = Integer.parseInt(br.readLine());
+
+        //call the recursive function to generate factorial
+        int result = fact(a);
+        System.out.println("Fatorial of " + a+  " is" +result);
+    }
+
+        static int fact(int b ){
+            if(b<=1)
+              return 1;
+            else
+              return(b* fact(b-1));
+        }
 }
