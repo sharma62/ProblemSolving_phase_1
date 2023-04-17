182a35d64c9d95c13d2b3bf064379d5dbaca3654
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2023-04-17 23:57:39 +0530

    solved percentage problem

--- CalcPercentage_19.java
+++ CalcPercentage_19.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class CalcPercentage_19 {
-    
+    public static void main(String[] args) {
+        double x = 0, y = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the value  of x");
+        x = scan.nextDouble();
+        System.out.println("Enter the value of y");
+        y = scan.nextDouble();
+        System.out.println();
+        System.out.println("Calculate percentage : (X of Y)");
+        System.out.println(x + "% of " + y + "is " + (x * y)/100);
+    }
 }
